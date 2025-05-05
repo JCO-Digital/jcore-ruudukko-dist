7bdb4afb10a53dfc044b02f4d88f95df9cdee5c3
foonly <niklas@uplink.fi>
2025-05-05 11:49:13 +0000

    Update main to output generated at 587360a

--- includes/notices.php
+++ includes/notices.php
@@ -13,18 +13,4 @@ namespace Jcore\Ruudukko;
  * @return void
  */
 function handle_notices(): void {
-
-		wp_admin_notice(
-			sprintf(
-			// translators: 1: Url to the options page.
-				__(
-					'Test error',
-					'jcore-ruudukko'
-				),
-				admin_url( 'admin.php?page=jcore-ruudukko-settings&tab=general' )
-			),
-			array(
-				'type' => 'error',
-			)
-		);
 }
