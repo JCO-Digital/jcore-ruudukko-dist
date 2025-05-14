f08ea4c27b2eb1cd525f73d430175771d02ae6a1
foonly <niklas@uplink.fi>
2025-05-14 12:23:56 +0000

    Update main to output generated at 19ffff5

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'jcodigital/jcore-ruudukko',
-        'pretty_version' => '0.1.0',
-        'version' => '0.1.0.0',
-        'reference' => null,
+        'pretty_version' => 'v0.2.3',
+        'version' => '0.2.3.0',
+        'reference' => '19ffff50ab3c2ebeb0f34db14ce486f559a6041d',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'jcodigital/jcore-ruudukko' => array(
-            'pretty_version' => '0.1.0',
-            'version' => '0.1.0.0',
-            'reference' => null,
+            'pretty_version' => 'v0.2.3',
+            'version' => '0.2.3.0',
+            'reference' => '19ffff50ab3c2ebeb0f34db14ce486f559a6041d',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
